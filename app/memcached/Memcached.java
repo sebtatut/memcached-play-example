09141748b125dec3428013e02ab04984a1cf6d88
Sebastian Tatut <tse@mikrosimage.eu>
2016-10-04 02:55:49 +0200

    Wrapped future.get in a CompletableFuture

--- app/memcached/Memcached.java
+++ app/memcached/Memcached.java
@@ -1,6 +1,8 @@
 package memcached;
 
+import java.util.concurrent.CompletionStage;
+
 public interface Memcached {
-	<T> T get(final String key);
+	<T> CompletionStage<T> get(final String key);
 	<T> void set(final String key, final T obj, int expiration);
 }
